adc2e9610f6c9bc701ffcfbaa082e1ec173b3b28
westorres9 <wester.torres9@gmail.com>
2022-04-05 09:20:17 -0300

    controller and add forms events

---
+++ js/controllers/form-controller.js
@@ -0,0 +1,69 @@
+import Address from '../controllers/address.js';
+
+
+function State() {
+
+    this.adress = new Address();
+    this.btnSave = null;
+    this.btnClear = null;
+
+    this.inputCep = null;
+    this.inputStreet = null;
+    this.inputNumber = null;
+    this.inputCity = null;
+
+    this.errorCep = null;
+    this.errorNumber = null;
+}
+
+
+const state = new State();
+
+
+export function init() {
+    state.inputCep = document.forms.newAddress.cep;
+    state.inputStreet = document.forms.newAddress.street;
+    state.inputNumber = document.forms.newAddress.number;
+    state.inputCity = document.forms.newAddress.city;
+
+    state.btnSave = document.forms.newAddress.btnSave;
+    state.btnClear = document.forms.newAddress.btnClear;
+
+    state.errorCep = document.querySelector('[data-error="cep"]');
+    state.errorNumber = document.querySelector('[data-error="number"]');
+
+    state.inputNumber.addEventListener('change', handleInputNumberChange);
+    state.btnClear.addEventListener('click', handleBtnClearClick);
+
+}
+
+function handleInputNumberChange(event) {
+    if (event.target.value =="") {
+        setFormError("number", "Campo requerido");
+    }
+    else {
+        setFormError("number", "");
+    };
+}
+
+function handleBtnClearClick(event) {
+    event.preventDefault();
+    clearForm();
+}
+
+function clearForm() {
+    state.inputCep.value = "";
+    state.inputStreet.value = "";
+    state.inputNumber.value = "";
+    state.inputCity.value = "";
+
+    setFormError("cep", "");
+    setFormError("number", "");
+    state.inputCep.focus();
+
+}
+
+function setFormError(key, value) {
+    const element = document.querySelector(`[data-error="${key}"]`);
+    element.innerHTML = value;
+}
